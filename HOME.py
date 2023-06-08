2e0e9df9675523bbe79b0eec2684054e19350989
AffandiSupriadi <122225654+AffandiSupriadi@users.noreply.github.com>
2023-06-08 20:00:26 +0700

    Add files via upload

---
+++ HOME.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+
+st.set_page_config(
+    page_title="Bipolar Junction Transistors AC Analysis",
+    page_icon="❤️",
+)
+
+
+st.header("Bipolar Junction Transistors AC Analysis")
+st.write("by: Affandi Supriadi")
+st.write("Dosen Pengampu: Ir. Rustamaji, M.T")
+st.sidebar.success("pilih perhitungan yang anda inginkan")
+st.write(
+    "Saya Affandi Supriadi dengan NRP 11-2021-020 mempersembahkan Aplikasi web perhitungan Analisis AC BJT. Untuk memenuhi tugas dari mata kuliah Elektrinika Analog. Silahkan klik tanda “>” disebelah kiri atas untuk memulai perhitungan."
+)
+st.write("---")
